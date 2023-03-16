e06d7a6ea1416b7d96add35935129f87b5fee3a1
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-03-17 04:08:04 +0700

    init

---
+++ pretter.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: "none",
+  printWidth: 100,
+  plugins: [
+    "prettier-plugin-svelte",
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  pluginSearchDirs: ["."],
+  tailwindConfig: "./tailwind.config.cjs",
+  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+};
---
+++ src/routes/forgot-password/+page.svelte
@@ -0,0 +1,1 @@
+Fogot Password
