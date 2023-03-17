616b745862d1e9407269f3c1911949df52d7f12d
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-03-17 09:31:23 +0700

    Setting up a Current Path Navigation

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import {page} from '$app/stores';
+</script>
+
+<header class="col-span-3 bg-daisyBush text-center">
+  <div>
+    <a href="/invoices">
+    </a>
+    <img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto"/>
+  </div>
+
+  <nav>
+    <ul class="font-bold">
+      <li><a href="/invoices" class:active={$page.url.pathname === '/invoices'}>Invoices</a></li>
+      <li>
+        <a href="/clients" class:active={$page.url.pathname === '/clients'}>Clients</a>
+      </li>
+      <li>
+        <a href="#">Settings</a>
+      </li>
+      <li>
+        <a href="#">Logout</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style lang="postcss">
+  nav ul li {
+    @apply mb-6;
+  }
+  nav ul li a {
+   @apply  font-bold text-white hover:text-goldenFizz text-2xl;
+  }
+  nav ul li a.active {
+   @apply text-robinEggBlue px-8 transition-[padding];
+   background: url('/images/active-nav--left.svg') left top no-repeat, url('/images/active-nav--right.svg') right top no-repeat;
+  }
+  nav ul li a.active:hover {
+   @apply px-9;
+  }
+</style>
