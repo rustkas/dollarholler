dedeedec8fc317f70d4a73f7d263c3740398eaf7
Anatolii Kosorukov <java1cprog@yandex.ru>
2023-03-18 13:28:55 +0700

    Page States on the Invoice List

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,8 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
+        arrowLeftUp: "url('/images/arrow-left-up.svg')",
+        arrowRightUp: "url('/images/arrow-right-up.svg')",
         circle: "url('/images/circle.svg')"
       },
       borderWidth: {
